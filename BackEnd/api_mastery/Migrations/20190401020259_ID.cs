84dd623901f135ede7a9ec17cd446cbb07e9362c
Matt <you@example.com>
2019-03-31 22:21:30 -0400

    Updated Models

---
+++ BackEnd/api_mastery/Migrations/20190401020259_ID.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api_mastery.Migrations
+{
+    public partial class ID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "GroupId",
+                table: "DogBreeds",
+                newName: "DogGroupId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DogGroupId",
+                table: "DogBreeds",
+                newName: "GroupId");
+        }
+    }
+}
